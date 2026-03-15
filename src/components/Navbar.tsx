0303a0266da3ca49b7c7b031e94aae70bcfca073
JEREM1997 <jeremie@krispykreme.ch>
2026-03-15 11:39:34 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -266,7 +266,7 @@ const Navbar: React.FC = () => {
         </div>
       </nav>
 
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
         <Outlet />
       </main>
     </>
